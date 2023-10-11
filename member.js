ba56f0aa239d1034eb70d76870455d57f8a43bdd
Mauricio Huayta Villanueva <82984226+maurihv13@users.noreply.github.com>
2023-10-11 20:19:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 30,
+        skills: ['JS', 'CSS', 'HTML']
+    };
+    console.log(skills.name);
+    console.log(skills.age);
+    console.log(skills.skills);
+    console.log(skills.skills[0]);
+    console.log(skills.skills[1]);
+    console.log(skills.skills[2]);
+}
